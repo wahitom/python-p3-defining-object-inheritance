44dcee5f14710c693012093b93c09b0593c6ad26
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-12-01 19:11:25 +0300

    Completed

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,22 @@
 from vehicle import Vehicle
 
+# Defining the superclass
+# Defining the subclass
+# overwriting inherited Methods 
+
 class Car(Vehicle):
-    pass
+   def go(self):
+      return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+# Asking the Car attrribute what its parent ie superclass is 
+print(Car.__bases__)
+
+# The __bases__ attribute is availabe for all python classes
+# even built ones like int 
+# ie 
+
+print(int.__bases__)
+
+# this is because pythn classes share the same metaclass ie type class
+
+print(int.__class__)
